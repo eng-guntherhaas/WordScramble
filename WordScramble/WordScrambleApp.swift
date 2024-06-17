2a7114ba2d0632d04b0ead433df2edc8446ade13
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-17 22:01:01 +0200

    initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gunther Masi Haas on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
